cb4e780621d5c79961736abb760f2f3e83a70aa3
Tatyana Kuznetsova <kuznetsova.t.s.nmc@gmail.com>
2024-12-13 19:21:04 +0500

    add second & third table

--- index.php
+++ index.php
@@ -20,87 +20,119 @@
                 <th>A && B</th>
                 <th>A xor B</th>
             </tr>
-            <tr>
-                <td>
-                    <?= $a = 0;?>
-                </td>
-                <td>
-                    <?= $b = 0;?>
-                </td>
-                <td>
-                    <?= (int)!$a;?>
-                </td>
-                <td>
-                    <?= (int)($a || $b);?>
-                </td>
-                <td>
-                    <?= (int)($a && $b);?>
-                </td>
-                <td>
-                    <?= (int)($a xor $b);?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <?= $a = 0;?>
-                </td>
-                <td>
-                    <?= $b = 1;?>
-                </td>
-                <td>
-                    <?= (int)!$a;?>
-                </td>
-                <td>
-                    <?= (int)($a || $b);?>
-                </td>
-                <td>
-                    <?= (int)($a && $b);?>
-                </td>
-                <td>
-                    <?= (int)($a xor $b);?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <?= $a = 1;?>
-                </td>
-                <td>
-                    <?= $b = 0;?>
-                </td>
-                <td>
-                    <?= (int)!$a;?>
-                </td>
-                <td>
-                    <?= (int)($a || $b);?>
-                </td>
-                <td>
-                    <?= (int)($a && $b);?>
-                </td>
-                <td>
-                    <?= (int)($a xor $b);?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <?= $a = 1;?>
-                </td>
-                <td>
-                    <?= $b = 1;?>
-                </td>
-                <td>
-                    <?= (int)!$a;?>
-                </td>
-                <td>
-                    <?= (int)($a || $b);?>
-                </td>
-                <td>
-                    <?= (int)($a && $b);?>
-                </td>
-                <td>
-                    <?= (int)($a xor $b);?>
-                </td>
-            </tr>
         </thead>
+        <tbody>
+            <?php
+                function echo_rows($x, $y) {
+                    echo '<tr>';
+                    echo '<td>'.$x.'</td>';
+                    echo '<td>'.$y.'</td>';
+                    echo '<td>'.(int)!$x.'</td>';
+                    echo '<td>'.(int)($x || $y).'</td>';
+                    echo '<td>'.(int)($x && $y).'</td>';
+                    echo '<td>'.(int)($x xor $y).'</td>';
+                    echo '</tr>';
+                }
+
+                $a = 0;
+                $b = 0;
+                echo_rows($a, $b);
+                $b = 1;
+                echo_rows($a, $b);
+                $a = 1;
+                $b = 0;
+                echo_rows($a, $b);
+                $b = 1;
+                echo_rows($a, $b);
+            ?>
+        </tbody>
+    </table>
+    <hr>
+
+    <h3>Таблица сравнения</h3>
+    <h4>Гибкое сравнение</h4>
+
+    <?php
+    function echo_bool($b) {
+        if ($b === true) return 'true';
+        else if ($b === false) return 'false';
+        else return 'null';
+    }
+    ?>
+
+    <table class="table table-secondary table-bordered">
+        <?php
+            $values = array("", true, false, 1, 0, -1, "1", null, "php");
+            for ($tr = 0; $tr<count($values); $tr++) {
+                if ($tr == 0) {
+                    echo '<thead>';
+                    echo '<tr>';
+                    echo '<td></td>';
+                    for ($td = 1; $td<count($values); $td++) {
+                        if ($values[$td] === true || $values[$td] === false || $values[$td] === null) 
+                            echo '<th>'.echo_bool($values[$td]).'</th>';
+                        else if (get_debug_type($values[$td]) == 'string') echo '<th>'.'"'.$values[$td].'"'.'</th>';
+                        else echo '<th>'.$values[$td].'</th>';
+                    }
+                    echo '</tr>';
+                    echo '</thead>';
+                    echo '<tbody>';
+                } else {                    
+                    echo '<tr>';
+                    $y = $values[$tr];
+                    if ($y === true || $y === false || $y === null) 
+                        echo '<th>'.echo_bool($y).'</th>';
+                    else if (get_debug_type($y) == 'string') 
+                        echo '<th>'.'"'.$y.'"'.'</th>';
+                    else echo '<th>'.$y.'</th>';
+                    for ($td = 1; $td<count($values); $td++) {
+                        $x = $values[$td];
+                        echo '<td>'.echo_bool($x == $y).'</td>';
+                    }
+                    echo '</tr>';
+                    
+                }
+            };
+            echo '</tbody>';
+        ?>
+    </table>
+
+    <h4>Жесткое сравнение</h4>
+
+    <table class="table table-secondary table-bordered">
+        <?php
+            $values = array("", true, false, 1, 0, -1, "1", null, "php");
+            for ($tr = 0; $tr<count($values); $tr++) {
+                if ($tr == 0) {
+                    echo '<thead>';
+                    echo '<tr>';
+                    echo '<td></td>';
+                    for ($td = 1; $td<count($values); $td++) {
+                        if ($values[$td] === true || $values[$td] === false || $values[$td] === null) 
+                            echo '<th>'.echo_bool($values[$td]).'</th>';
+                        else if (get_debug_type($values[$td]) == 'string') echo '<th>'.'"'.$values[$td].'"'.'</th>';
+                        else echo '<th>'.$values[$td].'</th>';
+                    }
+                    echo '</tr>';
+                    echo '</thead>';
+                    echo '<tbody>';
+                } else {
+                    echo '<tr>';
+                    $y = $values[$tr];
+                    if ($y === true || $y === false || $y === null) 
+                        echo '<th>'.echo_bool($y).'</th>';
+                    else if (get_debug_type($y) == 'string') 
+                        echo '<th>'.'"'.$y.'"'.'</th>';
+                    else echo '<th>'.$y.'</th>';
+                    for ($td = 1; $td<count($values); $td++) {
+                        $x = $values[$td];
+                        echo '<td>'.echo_bool($x === $y).'</td>';
+                    }
+                    echo '</tr>';
+                }
+            };
+            echo '</tbody>';
+        ?>
     </table>
 </body>
 </html>
